561df53219679d2d9b6d71590617f3da5f959f41
rakeshnaidu98 <rakeshnaidukandi@gmail.com>
2019-09-17 22:20:32 +0530

    Updates

---
+++ precache-manifest.27b9ff9c56db9ed2fb2cbb455e622bfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8713dc48af525637673865b25f49831",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "b56a38243f20cf204a9b",
+    "url": "/reactapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1555fcbf48e39322a99d",
+    "url": "/reactapp/static/js/2.d75131d5.chunk.js"
+  },
+  {
+    "revision": "b56a38243f20cf204a9b",
+    "url": "/reactapp/static/js/main.c59d5dff.chunk.js"
+  },
+  {
+    "revision": "b2b8833a44fa82a1fbc7",
+    "url": "/reactapp/static/js/runtime~main.4142c29b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
